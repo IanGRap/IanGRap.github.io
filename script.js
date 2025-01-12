4c5ba016ba80a090ac040dc9482d46ebac64b180
Ian Rapoport <iangrap@gmail.com>
2025-01-12 15:59:44 -0800

    fixed preloader

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
+
 function backToTop(){
 	document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-<script>
-    var loader = document.getElementById("preloader");
-
-    window.addEventListener("load", function(){
-        loader.style.display = "none";
-    })
-</script>
