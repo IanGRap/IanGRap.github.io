cac13ea654146ed3e69aecd8960152a2327baa7e
Ian Rapoport <iangrap@gmail.com>
2025-01-12 15:48:33 -0800

    hiding loader

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function backToTop(){
 	document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
