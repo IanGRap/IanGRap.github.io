bf0e867e41fe4f8332731d77eb99ddd42f777cbb
Ian Rapoport <iangrap@gmail.com>
2025-01-12 16:49:07 -0800

    one last test

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var preloader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    preloader.style.display = "none";
+})
+
 function backToTop(){
 	document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
