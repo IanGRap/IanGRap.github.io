280086b8e7cb395768dad2fa961377521f0ad176
Ian Rapoport <iangrap@gmail.com>
2025-01-12 16:14:28 -0800

    moved script into html file

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-<script>
-
-    var loader = document.getElementById("preloader");
-
-    window.addEventListener("load", function(){
-        preloader.style.display = "none";
-    })
-
-    function backToTop(){
-	    document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
-
-</script>
+function backToTop(){
+	document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
